cc296683b89d51454ac348dfadaa208d14d71067
murilosaca <123023601+murilosaca@users.noreply.github.com>
2023-07-10 09:47:58 -0300

    Update exercicio_11.py

--- exercicio_11.py
+++ exercicio_11.py
@@ -1,3 +1,8 @@
+# código que peça 2 números inteiros e um número real. Calcule e mostre:
+# a = o produto do dobro do primeiro com metade do segundo .
+# b= a soma do triplo do primeiro com o terceiro.
+# c = terceiro elevado ao cubo.
+
 a = int(input("Insira um número inteiro:"))
 b = int(input("Insira outro número inteiro:"))
 c = int(input("Insira um número natural:"))
