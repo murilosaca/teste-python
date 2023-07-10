513256dcd4162a27216807817e15cb20c5809d08
murilosaca <123023601+murilosaca@users.noreply.github.com>
2023-07-10 09:55:38 -0300

    Update exercicio_18.py

--- exercicio_18.py
+++ exercicio_18.py
@@ -1,3 +1,8 @@
+# Faça um programa que peça o tamanho de um arquivo para download (em MB) e
+# a velocidade de um link de Internet (em Mbps), calcule e informe o tempo 
+# aproximado de download do arquivo usando este link (em minutos).
+
+
 aq = int(input("insira o tamanho do arquivo:"))
 velo = int(input("insira a velocidade de um link da internet:"))
 td = aq/velo
